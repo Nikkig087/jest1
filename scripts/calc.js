ba36bfd8f4b1cd8bdf165c6346cb768f94beed3b
NikkiGetsee <Nicola.getsee@gmail.com>
2024-06-26 20:28:02 +0000

    added in check to make sure a number is entered

--- scripts/calc.js
+++ scripts/calc.js
@@ -1,6 +1,11 @@
 function addition(num1,num2) {
     let sum = num1 + num2
     return sum;
+   return (isNaN(num1) || isNaN(num2)) ? "Inputs must be numbers" : num1 + num2;
+   /**Ternary Operator: This single line of code uses the ternary        
+    *  operator to perform the check and return the result.
+       If isNaN(num1) or isNaN(num2) is true (meaning one of the inputs is not a number), it returns the string "Inputs must be numbers".
+        Otherwise, it returns the sum of num1 and num2. */
 }
 
 module.exports = addition;
--- scripts/tests/calc.test.js
+++ scripts/tests/calc.test.js
@@ -7,7 +7,7 @@ describe("Calculator", () => {
             expect(addition(20, 22)).toBe(42);
         });
         test("should return 73 for 42 + 31", () => {
-            expect(addition(42, 31)).toBe(73);
+            expect(addition(n, 31)).toBe(73);
         });
     });
     describe("Subtraction function", ()=>{
